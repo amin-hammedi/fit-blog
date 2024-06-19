546776a87b3a425dda867cebdbf59719c234f9b5
amin hammedi <aminhammedi002@gmail.com>
2024-06-19 18:14:35 +0100

    spe

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Analytics></Analytics>
+      <SpeedInsights></SpeedInsights>
       <body className={inter.className}>{children}</body>
     </html>
   );
